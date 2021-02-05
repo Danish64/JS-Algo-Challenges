bceac53ca41ec86a9a9216e51569fd668a52c27f
Hashir524 <hashirsaeed64@gmail.com>
2021-02-06 04:05:47 +0500

    Adjusted Files

--- absoluteValuesSumMinization/absoluteValuesSumMinimization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinimization.ts
@@ -3,8 +3,3 @@ export function absoluteValuesSumMinimization(a: number[]): number {
 
   return isEven ? a[a.length / 2 - 1] : a[Math.floor(a.length / 2)];
 }
-
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
