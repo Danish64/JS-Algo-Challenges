770ba2f5364bd145dfaab4e9efca151d0a45c178
Danish64 <danishali0051@gmail.com>
2021-02-09 17:25:14 +0500

    Alternating Sums Algorithm Done

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,16 @@
 export function alternatingSums(a: number[]): number[] {
+  let team1Weight: number = a[0];
+  let team2Weight: number = a[1];
 
+  for (let i = 2; i < a.length; i++) {
+    if (i % 2 === 0) {
+      team1Weight = team1Weight + a[i];
+    } else {
+      team2Weight = team2Weight + a[i];
+    }
+  }
+
+  return [team1Weight, team2Weight];
 }
 
-console.log(alternatingSums([50, 60, 60, 45, 70]))
+console.log(alternatingSums([50, 60, 60, 45, 70]));
