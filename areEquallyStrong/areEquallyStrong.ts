0ddf09d8f5c5191b6f35eddf1a5c59b0308f419e
Danish64 <danishali0051@gmail.com>
2021-02-10 15:36:03 +0500

    areEquallyStrong Algorithm

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,12 @@
-export function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
-
+export function areEquallyStrong(
+  yourLeft: number,
+  yourRight: number,
+  friendsLeft: number,
+  friendsRight: number
+): boolean {
+  return yourLeft + yourRight === friendsLeft + friendsRight;
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10));
+console.log(areEquallyStrong(10, 15, 15, 9));
+console.log(areEquallyStrong(10, 15, 15, 10));
