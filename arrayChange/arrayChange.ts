0c576ec60b9a475698ebd9b12ca3cc46b340d597
Danish64 <danishali0051@gmail.com>
2021-02-15 14:23:44 +0500

    Array Change Algorithm Implemented

--- arrayChange/arrayChange.ts
+++ arrayChange/arrayChange.ts
@@ -1,5 +1,16 @@
 export function arrayChange(inputArray: number[]): number {
+  let count = 0;
 
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i] >= inputArray[i + 1]) {
+      const difference = inputArray[i] + 1 - inputArray[i + 1];
+      inputArray[i + 1] = inputArray[i] + 1;
+      count += difference;
+    }
+  }
+
+  return count;
 }
 
-// console.log(arrayChange([1, 1, 1]));
+console.log(arrayChange([1, 1, 1]));
+console.log(arrayChange([2, 1, 1]));
