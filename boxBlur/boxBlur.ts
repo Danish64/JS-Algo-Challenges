d82ab90407123893d79d4701d0fc06725e03885b
Danish64 <danishali0051@gmail.com>
2021-02-19 15:26:34 +0500

    Box Blur Algorithm Started

--- boxBlur/boxBlur.ts
+++ boxBlur/boxBlur.ts
@@ -1,7 +1,23 @@
 export function boxBlur(image: number[][]): number[][] {
-  const average: number = 0;
+  const res = [];
 
-  return [[average]];
+  for (let y = 0; y < image.length - 2; y++) {
+    const line = [];
+    for (let x = 0; x < image[y].length - 2; x++) {
+      let sum = 0;
+      let count = 0;
+      for (let a = y; a < y + 3; a++) {
+        for (let b = x; b < x + 3; b++) {
+          sum += image[a][b];
+          count++;
+        }
+      }
+      line.push(Math.floor(sum / count));
+    }
+    res.push(line);
+  }
+
+  return res;
 }
 
 console.log(
