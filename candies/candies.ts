2959a29b1fa1a1a3710f4fad80464e51d0630557
Danish64 <danishali0051@gmail.com>
2021-02-21 22:54:34 +0500

    Candies made efficient

--- candies/candies.ts
+++ candies/candies.ts
@@ -1,5 +1,5 @@
-export function candies(n: number, m: number): number{
-
-    
-
+export function candies(n: number, m: number): number {
+  return Math.floor(m / n) * n;
 }
+
+console.log(candies(3, 10));
