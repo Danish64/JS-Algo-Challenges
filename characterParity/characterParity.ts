7bd0a35479f7fd71e373e4860d3f03013c46039c
Danish64 <danishali0051@gmail.com>
2021-02-22 15:26:56 +0500

    characterParity Solved

--- characterParity/characterParity.ts
+++ characterParity/characterParity.ts
@@ -1,7 +1,13 @@
 export function characterParity(symbol: string): string {
-
+  if (isNaN(symbol)) {
+    return "not a digit";
+  }
+  if (parseInt(symbol) % 2 === 0) {
+    return "even";
+  }
+  return "odd";
 }
 
-// console.log(characterParity('5'))
-// console.log(characterParity('8'))
-// console.log(characterParity('q'))
+console.log(characterParity("q"));
+console.log(characterParity("5"));
+console.log(characterParity("8"));
