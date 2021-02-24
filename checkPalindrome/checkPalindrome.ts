d3b72eb9edc501471c98fb675d19413af7f2eff2
Danish64 <danishali0051@gmail.com>
2021-02-24 12:39:52 +0500

    Check Palindrome Solved

--- checkPalindrome/checkPalindrome.ts
+++ checkPalindrome/checkPalindrome.ts
@@ -1,7 +1,16 @@
 export function checkPalindrome(inputString: string): boolean {
+  let isPalindrome = false;
 
+  if (
+    inputString.toLowerCase().split("").reverse().join("") ===
+    inputString.toLowerCase()
+  ) {
+    isPalindrome = true;
+  }
+
+  return isPalindrome;
 }
 
-// console.log(checkPalindrome('aabaa'));
-// console.log(checkPalindrome('abac'));
-// console.log(checkPalindrome('a'));
+console.log(checkPalindrome("a"));
+console.log(checkPalindrome("aabaa"));
+console.log(checkPalindrome("abac"));
