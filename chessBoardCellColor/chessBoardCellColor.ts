a68f53713fd850219716700648008c0bae163023
Danish64 <danishali0051@gmail.com>
2021-02-24 15:06:38 +0500

    ChessBoardCellColor Solved

--- chessBoardCellColor/chessBoardCellColor.ts
+++ chessBoardCellColor/chessBoardCellColor.ts
@@ -1,6 +1,26 @@
 export function chessBoardCellColor(cell1: string, cell2: string): boolean {
+  const Board = {
+    A: 1,
+    B: 2,
+    C: 3,
+    D: 4,
+    E: 5,
+    F: 6,
+    G: 7,
+    H: 8,
+  };
 
+  const cell1Sum = Board[cell1[0]] + parseInt(cell1[1]);
+  const cell2Sum = Board[cell2[0]] + parseInt(cell2[1]);
+
+  if (cell1Sum % 2 === 0 && cell2Sum % 2 === 0) {
+    return true;
+  } else if (cell1Sum % 2 !== 0 && cell2Sum % 2 !== 0) {
+    return true;
+  }
+
+  return false;
 }
 
-// console.log(chessBoardCellColor('A1', 'C3'));
-// console.log(chessBoardCellColor('A1', 'H3'));
+console.log(chessBoardCellColor("A1", "C3"));
+console.log(chessBoardCellColor("A1", "H3"));
