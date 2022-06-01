a9c19b21ce2d03550ca414b1f99cd9c6d53f2a5c
Danish64 <danishali0051@gmail.com>
2022-06-02 02:32:31 +0500

    changes: started again :)

--- absoluteValuesSumMinization/absoluteValuesSumMinimization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinimization.ts
@@ -1,5 +1,10 @@
-export function absoluteValuesSumMinimization(a: number[]): number {
-  const isEven = a.length % 2 === 0;
+const absValueSumMinimization = (a: number[]): number => {
+    const isEven = a.length % 2 === 0;
 
-  return isEven ? a[a.length / 2 - 1] : a[Math.floor(a.length / 2)];
+    return isEven ? a[a.length / 2 - 1] : a[Math.floor(a.length / 2)];
 }
+
+console.log(absValueSumMinimization([2, 4, 7]));
+console.log(absValueSumMinimization([2, 4, 7, 6]));
+console.log(absValueSumMinimization([2, 4, 7, 6, 6]));
+console.log(absValueSumMinimization([2, 4, 7, 6, 6, 8]));
